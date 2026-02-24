d9e7e73e5f783437c6aea623db2ebfba046f7e37
USER <andreinwose@gmail.com>
2026-02-24 13:03:10 +0100

    project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { Navbar } from "@/layout/Navbar";
-// import { Hero } from "@/sections/Hero";
-// import { About } from "@/sections/About";
-// import { Projects } from "@/sections/Projects";
-// import { Experience } from "@/sections/Experience";
-// import { Testimonials } from "@/sections/Testimonials";
-// import { Contact } from "@/sections/Contact";
+
 import { Footer } from "./layout/Footer";
 import { Hero } from "./sections/Hero";
+import { About } from "./sections/About";
+import { Projects } from "./sections/Projects";
 
 function App() {
   return (
@@ -14,12 +11,8 @@ function App() {
       <Navbar />
       <main>
         <Hero />
-        {/* <Hero />
         <About />
         <Projects />
-        <Experience />
-        <Testimonials />
-        <Contact /> */}
       </main>
       {/* <Footer /> */}
     </div>
